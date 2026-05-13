42c9ff58e7265da88c4cebd49ac77c6182ce1bb1
PYAE SONE <156494651+pyae198022@users.noreply.github.com>
2026-05-13 15:45:59 +0630

    Time: 1 ms (97.92%), Space: 42.6 MB (60.11%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x < 2) {
+            return x;
+        }
+
+        int left = 0;
+        int right = x / 2;
+        int ans = 0;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            long m = (long) mid * mid;
+
+            if(m == x) {
+                return mid;
+            }else if(m < x) {
+                ans = mid;
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+
+    }
+}
+
+/* 
+0 - x
+0 - 4
+unknow number = (m * m) <= x 
+*/
